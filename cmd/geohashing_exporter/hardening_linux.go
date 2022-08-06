1415fc379d33bce2aa2530bc8b7958096a527567
Alvar Penning <post@0x21.biz>
2022-08-06 18:28:01 +0200

    geohashing_exporter: Landlock on Linux

--- cmd/geohashing_exporter/hardening_linux.go
+++ cmd/geohashing_exporter/hardening_linux.go
@@ -6,15 +6,73 @@ package main
 
 import (
 	"log"
+	"os"
 	"strings"
+
+	_ "unsafe" // go:linkname
+
+	"github.com/landlock-lsm/go-landlock/landlock"
+	llsys "github.com/landlock-lsm/go-landlock/landlock/syscall"
 
 	syscallset "github.com/oxzi/syscallset-go"
 )
 
-// toLeastPrivilege with the help of Linux' seccomp-bpf.
-func toLeastPrivilege() {
+//go:linkname zoneSources time.zoneSources
+var zoneSources []string
+
+// toLeastPrivilegeLandlock limits with Linux' Landlock.
+//
+// Turns out, Golang's time package loads timezone information during its
+// initialization functions and sources those from different locations. On top,
+// there might be an additional path from downstream distribution patches.
+//
+//   https://cs.opensource.google/go/go/+/master:src/time/zoneinfo_unix.go;bpv=0;bpt=0
+//
+// Thus, we access this not exported variable, filter for path validity as
+// go-landlock returns an error otherwise. I just want to have unveil(2)..
+func toLeastPrivilegeLandlock() {
+	_, err := llsys.LandlockGetABIVersion()
+	if err != nil {
+		log.Printf("Landlock is not supported.")
+		return
+	}
+
+	allowedZoneSourceDirs := []string{}
+	allowedZoneSourceFiles := []string{}
+	for _, zoneSource := range zoneSources {
+		info, err := os.Stat(zoneSource)
+		if err != nil {
+			continue
+		} else if info.IsDir() {
+			allowedZoneSourceDirs = append(allowedZoneSourceDirs, zoneSource)
+		} else {
+			allowedZoneSourceFiles = append(allowedZoneSourceFiles, zoneSource)
+		}
+	}
+
+	err = landlock.V2.BestEffort().RestrictPaths(
+		// Golang's time package
+		landlock.ROFiles("/etc/localtime"),
+		landlock.ROFiles(allowedZoneSourceFiles...),
+		landlock.RODirs(allowedZoneSourceDirs...),
+
+		// Golang's net/http HTTP server and lookup
+		landlock.RODirs("/proc/sys"),
+		landlock.ROFiles(
+			"/etc/hosts",
+			"/etc/nsswitch.conf",
+			"/etc/resolv.conf",
+		),
+	)
+	if err != nil {
+		log.Fatalf("Cannot apply Landlock filter: %v", err)
+	}
+}
+
+// toLeastPrivilegeSeccompBpf limits with Linux' seccomp-bpf.
+func toLeastPrivilegeSeccompBpf() {
 	if !syscallset.IsSupported() {
-		log.Print("seccomp-bpf or syscallset is not supported")
+		log.Print("seccomp-bpf or syscallset is not supported.")
 		return
 	}
 
@@ -26,10 +84,17 @@ func toLeastPrivilege() {
 		"@network-io",
 		"@process ~execve ~execveat ~fork ~kill",
 		"@signal",
+		"@system-service",
 		"fadvise64 ioctl madvise mremap sysinfo",
 	}
 	err := syscallset.LimitTo(strings.Join(filter, " "))
 	if err != nil {
 		log.Fatalf("Cannot apply syscallset resp. seccomp-bpf filter: %v", err)
 	}
+}
+
+// toLeastPrivilege is achieved on a Linux with Landlock and seccomp-bpf.
+func toLeastPrivilege() {
+	toLeastPrivilegeLandlock()
+	toLeastPrivilegeSeccompBpf()
 }
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ go 1.17
 
 require (
 	github.com/hashicorp/golang-lru v0.5.4
+	github.com/landlock-lsm/go-landlock v0.0.0-20220804194411-c936b994be1b
 	github.com/oxzi/syscallset-go v0.1.0
 	github.com/prometheus/client_golang v1.13.0
 )
@@ -25,4 +26,5 @@ require (
 	golang.org/x/net v0.0.0-20220805013720-a33c5aa5df48 // indirect
 	golang.org/x/sys v0.0.0-20220804214406-8e32c043e418 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
+	kernel.org/pub/linux/libs/security/libcap/psx v1.2.65 // indirect
 )
